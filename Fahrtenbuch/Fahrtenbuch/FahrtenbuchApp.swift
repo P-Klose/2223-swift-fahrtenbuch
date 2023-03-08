d3ae2f0508b40aa3ae82e47be06958a680314ee2
P-Klose <ipad@klose.at>
2023-03-08 12:32:59 +0100

    initial commit

---
+++ Fahrtenbuch/Fahrtenbuch/FahrtenbuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FahrtenbuchApp.swift
+//  Fahrtenbuch
+//
+//  Created by Peter Klose on 08.03.23.
+//
+
+import SwiftUI
+
+@main
+struct FahrtenbuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
