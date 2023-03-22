8c1ee2932b4205a38232cee3e5fcd4cb507b0d18
P-Klose <ipad@klose.at>
2023-03-22 08:50:34 +0100

    fix infinity loop

--- Fahrtenbuch/Fahrtenbuch/ContentView.swift
+++ Fahrtenbuch/Fahrtenbuch/ContentView.swift
@@ -13,7 +13,7 @@ class BluetoothViewModel: NSObject, CBCentralManagerDelegate, ObservableObject {
     
     let centralManager = CBCentralManager(delegate: nil, queue: nil)
     private var peripherals: [CBPeripheral] = []
-    let targetDeviceUUID = UUID(uuidString: "TARGET_DEVICE_UUID_DAAA")!
+    let targetDeviceUUID = UUID(uuidString: "TARGET_DEVICE_UUID_DAAA")
     @Published var peripheralNames: [String] = []
     
     override init() {
@@ -22,59 +22,57 @@ class BluetoothViewModel: NSObject, CBCentralManagerDelegate, ObservableObject {
     }
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-            if central.state == .poweredOn {
-                centralManager.scanForPeripherals(withServices: nil, options: nil)
-            }
+        if central.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
         }
-
-        func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-            if peripheral.identifier == targetDeviceUUID {
-                centralManager.stopScan()
-                showNotification()
-            }
-        }
-
-        func showNotification() {
-            let content = UNMutableNotificationContent()
-            content.title = "Bluetooth Connection Established"
-            content.body = "You are now connected to the target Bluetooth device"
-            let request = UNNotificationRequest(identifier: "bluetoothConnection", content: content, trigger: nil)
-            UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-        }
+    }
     
-    let bluetoothManager = BluetoothViewModel()
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.identifier == targetDeviceUUID {
+            centralManager.stopScan()
+            showNotification()
+        }
+    }
+    
+    func showNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Bluetooth Connection Established"
+        content.body = "You are now connected to the target Bluetooth device"
+        let request = UNNotificationRequest(identifier: "bluetoothConnection", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
     
 }
 
 
 /*
-extension BluetoothViewModel: CBCentralManagerDelegate {
-    
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        
-        if central.state == .poweredOn {
-            self.centralManager?.scanForPeripherals(withServices: nil)
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        
-        if peripheral.identifier == targetDeviceUUID {
-            centralManager?.stopScan()
-                    showNotification()
-                }
-        
-       /* if !peripherals.contains(peripheral) {
-            self.peripherals.append(peripheral)
-            self.peripheralNames.append(peripheral.name ?? "unnamed device")
-        }*/
-        
-    }
-    
-    
-    
-}
-*/
+ extension BluetoothViewModel: CBCentralManagerDelegate {
+ 
+ func centralManagerDidUpdateState(_ central: CBCentralManager) {
+ 
+ if central.state == .poweredOn {
+ self.centralManager?.scanForPeripherals(withServices: nil)
+ }
+ }
+ 
+ func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+ 
+ if peripheral.identifier == targetDeviceUUID {
+ centralManager?.stopScan()
+ showNotification()
+ }
+ 
+ /* if !peripherals.contains(peripheral) {
+  self.peripherals.append(peripheral)
+  self.peripheralNames.append(peripheral.name ?? "unnamed device")
+  }*/
+ 
+ }
+ 
+ 
+ 
+ }
+ */
 struct ContentView: View {
     @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
     
@@ -84,20 +82,20 @@ struct ContentView: View {
                 peripheral in Text(peripheral)
             }
             .navigationTitle("Peripherals")
-    }
-    
-    /*@ObservedObject var viewModel: ViewModel
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
         }
-        .padding()
-        .task {
-            viewModel.checkForPremission()
-        }*/
+        
+        /*@ObservedObject var viewModel: ViewModel
+         var body: some View {
+         VStack {
+         Image(systemName: "globe")
+         .imageScale(.large)
+         .foregroundColor(.accentColor)
+         Text("Hello, world!")
+         }
+         .padding()
+         .task {
+         viewModel.checkForPremission()
+         }*/
     }
 }
 
