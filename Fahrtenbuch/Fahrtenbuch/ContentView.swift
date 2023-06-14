18cd24845998a149c8dba7cc289f46737f176c6c
P-Klose <ipad@klose.at>
2023-06-14 14:28:10 +0200

    feat(Color): update colors

--- Fahrtenbuch/Fahrtenbuch/ContentView.swift
+++ Fahrtenbuch/Fahrtenbuch/ContentView.swift
@@ -11,29 +11,30 @@ struct ContentView: View {
     @ObservedObject var vehicleViewModel: VehicleViewModel
     @ObservedObject var expenseViewModel: ExpenseViewModel
     @ObservedObject var mapViewModel: MapViewModel
+    @State private var tabColor: Color = .accentColor
     var body: some View {
         TabView {
             ViewExpenses(expenseViewModel: expenseViewModel, vehicleViewModel: vehicleViewModel)
                 .tabItem() {
                     Image(systemName: "eurosign.circle")
                     Text("Ausgaben")
-                }
+                }.onAppear(perform: { tabColor = .green })
             ViewVehicle(vehicleViewModel: vehicleViewModel)
                 .tabItem() {
                     Image(systemName: "car.2")
                     Text("Fahrzeuge")
-                }
+                }.onAppear(perform: { tabColor = .pink })
             ViewRides(mapViewModel: mapViewModel, vehicleViewModel: vehicleViewModel)
                 .tabItem() {
                     Image(systemName: "map")
                     Text("Aufzeichnen")
-                }
+                }.onAppear(perform: { tabColor = .blue })
             ViewTrips(tvm: mapViewModel.tripViewModel, vvm: vehicleViewModel)
                 .tabItem {
                     Image(systemName: "road.lanes.curved.right")
                     Text("Fahrten")
-                }
-        }
+                }.onAppear(perform: { tabColor = .pink })
+        }.tint(tabColor)
         
     }
 }
